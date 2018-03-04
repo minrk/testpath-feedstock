3ab904781e0f96118f10a083cdc1bc6c690ebecf
Min RK <benjaminrk@gmail.com>
2018-03-04 12:44:07 +0100

    rerender with conda-smithy@7028ea39ae3564035d8d93ce2ca01e53e252ed3f
    fixes export of HOST_USER_ID for use with `docker run -e`

--- .circleci/run_docker_build.sh
+++ .circleci/run_docker_build.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
 RECIPE_ROOT=$FEEDSTOCK_ROOT/recipe
 # ensure BINSTAR_TOKEN is defined even if empty
-BINSTAR_TOKEN=${BINSTAR_TOKEN:-}
+export BINSTAR_TOKEN=${BINSTAR_TOKEN:-}
 
 set -x
 docker info
@@ -32,11 +32,11 @@ CONDARC
 # In order for the conda-build process in the container to write to the mounted
 # volumes, we need to run with the same id as the host machine, which is
 # normally the owner of the mounted volumes, or at least has write permission
-HOST_USER_ID=$(id -u)
+export HOST_USER_ID=$(id -u)
 # Check if docker-machine is being used (normally on OSX) and get the uid from
 # the VM
 if hash docker-machine 2> /dev/null && docker-machine active > /dev/null; then
-    HOST_USER_ID=$(docker-machine ssh $(docker-machine active) id -u)
+    export HOST_USER_ID=$(docker-machine ssh $(docker-machine active) id -u)
 fi
 
 ARTIFACTS="$FEEDSTOCK_ROOT/build_artifacts"
